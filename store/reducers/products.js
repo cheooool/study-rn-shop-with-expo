31c6545a9ca4c4085be810e8d748d8c4ca3103b5
cheooool <g.cheooool@gmail.com>
2021-03-01 19:07:05 +0900

    첫 커밋
    초기 패키지 설치 및 파일 구조 구성 후

---
+++ store/reducers/products.js
@@ -0,0 +1,11 @@
+import PRODUCTS from '../../data/dummy-data';
+
+const initialState = {
+  availableProducts: PRODUCTS,
+  userProducts: PRODUCTS.filter((prod) => prod.ownerID === 'u1'),
+};
+const productsReducer = (state = initialState, action) => {
+  return state;
+};
+
+export default productsReducer;
